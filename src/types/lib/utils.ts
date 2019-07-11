26a32f49b59254a9370be9cd7fc38bc3f85ab25c
Blaine Hansen <faichenshing@gmail.com>
2019-07-10 19:34:15 -0600

    adding fancy functional types and folding functions

--- src/types/lib/utils.ts
+++ src/types/lib/utils.ts
@@ -19,3 +19,73 @@ export type Unshift<Item, List extends any[]> =
 // 		return [this.val, ...rest] as Unshift<A, L>
 // 	}
 // }
+
+
+
+type Head<L extends any[]> =
+	L extends [any, ...any[]]
+	? L[0]
+	: never
+
+type A = Head<[string, number]>
+
+type Tail<L extends any[]> =
+	((...l: L) => unknown) extends ((_: any, ...tail: infer R) => unknown)
+	? R
+	: []
+
+type HasTail<L extends any[]> =
+	L extends ([] | [any])
+	? false
+	: true
+
+type Last<L extends any[]> = {
+	0: Last<Tail<L>>
+	1: Head<L>
+}[
+	HasTail<L> extends true
+	? 0
+	: 1
+]
+
+type OnlyOne<L extends any[]> =
+	L extends [any]
+	? true
+	: false
+
+type Func = (arg: any) => any
+
+type SingleParameter<F extends Func> =
+	F extends (arg: infer R) => any
+	? R
+	: never
+
+type ReturnToArg<R extends Func, A extends Func> =
+	SingleParameter<A> extends ReturnType<R>
+	? true
+	: false
+
+type F = ReturnToArg<() => string, (arg: string) => void>
+
+type S = SingleParameter<(args: any[]) => any>
+
+
+type FoldingFunctions<L extends Func[]> = {
+	// base case
+	0: ReturnType<L[0]>
+	// recursive case
+	1: FoldingFunctions<Tail<L>>
+	// failure case
+	2: never
+}[
+	HasTail<L> extends true
+	? ReturnToArg<Head<L>, Head<Tail<L>>> extends true
+		? 1
+		: 2
+	: OnlyOne<L> extends true
+		? 0
+		: 2
+]
+
+type Z = FoldingFunctions<[() => string, (v: number) => boolean]>
+
