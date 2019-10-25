5f936f608dfad207ab7e4aeb60cb2720bd401a5e
Blaine Hansen <faichenshing@gmail.com>
2019-10-25 13:00:33 -0600

    adding a test script

---
+++ test.sh
@@ -0,0 +1,29 @@
+set -e
+
+npx lerna exec --scope @ts-std/types -- npm test
+npx lerna exec --scope @ts-std/types -- npm run build
+npx lerna link
+
+npx lerna exec --scope @ts-std/monads -- npm test
+npx lerna exec --scope @ts-std/monads -- npm run build
+npx lerna link
+
+npx lerna exec --scope @ts-std/extensions -- npm test
+npx lerna exec --scope @ts-std/extensions -- npm run build
+npx lerna link
+
+npx lerna exec --scope @ts-std/collections -- npm test
+npx lerna exec --scope @ts-std/collections -- npm run build
+npx lerna link
+
+# npx lerna exec --scope @ts-std/codec -- npm test
+# npx lerna exec --scope @ts-std/codec -- npm run build
+# npx lerna link
+
+# npx lerna exec --scope @ts-std/enum -- npm test
+# npx lerna exec --scope @ts-std/enum -- npm run build
+# npx lerna link
+
+# npx lerna exec --scope @ts-std/machine -- npm test
+# npx lerna exec --scope @ts-std/machine -- npm run build
+# npx lerna link
