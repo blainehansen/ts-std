b9c3a80259026064956f382ae416e41c2802d553
Blaine Hansen <faichenshing@gmail.com>
2023-05-03 13:25:28 -0600

    adding set to_array functions

---
+++ src/extensions/lib/set.spec.ts
@@ -0,0 +1,22 @@
+import 'mocha'
+import './set'
+import { expect } from 'chai'
+
+describe('to_array', () => {
+	it('works', () => {
+		expect((new Set()).to_array()).eql([])
+	})
+})
+
+describe('map_to_array', () => {
+	it('works', () => {
+		expect((new Set()).map_to_array(() => null)).eql([])
+
+		expect((new Set([1, 2])).map_to_array(() => null)).eql([null, null])
+
+		const inc = (n: number) => n + 1
+		expect((new Set([1, 1, 2])).map_to_array(inc)).eql([2, 3])
+		expect((new Set([1, 1, 2, 2])).map_to_array(inc)).eql([2, 3])
+		expect((new Set([1, 1, 2, 2])).map_to_array(inc)).eql([2, 3])
+	})
+})
---
+++ src/extensions/lib/set.ts
@@ -0,0 +1,21 @@
+export type MapFunc<T, U> = (element: T) => U
+
+declare global {
+	interface Set<T> {
+		to_array<U>(this: Set<T>): T[]
+		map_to_array<U>(this: Set<T>, fn: MapFunc<T, U>): T[]
+	}
+}
+
+Set.prototype.to_array = function<T, U>(this: Set<T>): T[] {
+	return Array.from(this)
+}
+
+Set.prototype.map_to_array = function<T, U>(this: Set<T>, fn: MapFunc<T, U>): U[] {
+	const give = [] as U[]
+	for (const element of this) {
+		give.push(fn(element))
+	}
+
+	return give
+}
